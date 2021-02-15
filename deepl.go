c5a9b110a1ae9d305b116fad5d9b663d4977b303
Saman Hosseini <bounoable@gmail.com>
2021-02-15 15:17:42 +0100

    docs: update docs

--- deepl.go
+++ deepl.go
@@ -17,7 +17,90 @@ const (
 	V2 = "https://api.deepl.com/v2"
 )
 
-// New returns a usable deepl client.
+// A Client is a deepl client.
+type Client struct {
+	client       httpi.Client
+	authKey      string
+	baseURL      string
+	translateURL string
+}
+
+// A ClientOption configures a Client.
+type ClientOption func(*Client)
+
+// A TranslateOption configures a translation request.
+type TranslateOption func(url.Values)
+
+// Error is a DeepL error.
+type Error struct {
+	// The HTTP error code, returned by the deepl API.
+	Code int
+}
+
+// BaseURL returns a ClientOption that sets the base url for requests.
+func BaseURL(url string) ClientOption {
+	return func(c *Client) {
+		c.baseURL = url
+	}
+}
+
+// HTTPClient returns a ClientOption that specifies the http.Client that's used
+// when making requests.
+func HTTPClient(client httpi.Client) ClientOption {
+	return func(c *Client) {
+		c.client = client
+	}
+}
+
+// SourceLang returns a ClientOption that specifies the source language of the
+// input text. If SourceLang is not used, DeepL automatically figures out the
+// source language.
+func SourceLang(lang Language) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("source_lang", string(lang))
+	}
+}
+
+// SplitSentences returns a TranslateOption that sets the `split_sentences`
+// DeepL option.
+func SplitSentences(split SplitSentence) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("split_sentences", split.Value())
+	}
+}
+
+// PreserveFormatting returns a TranslateOption that sets the
+// `preserve_formatting` DeepL option.
+func PreserveFormatting(preserve bool) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("preserve_formatting", boolString(preserve))
+	}
+}
+
+// Formality returns a TranslateOption that sets the `formality` DeepL option.
+func Formality(formal Formal) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("formality", formal.Value())
+	}
+}
+
+// TagHandling returns a TranslateOption that sets the `tag_handling` DeepL
+// option.
+func TagHandling(handling TagHandlingStrategy) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("tag_handling", handling.Value())
+	}
+}
+
+// IgnoreTags returns a TranslateOption that sets the `ignore_tags` DeepL
+// option.
+func IgnoreTags(tags ...string) TranslateOption {
+	return func(vals url.Values) {
+		vals.Set("ignore_tags", strings.Join(tags, ","))
+	}
+}
+
+// New returns a Client that uses authKey as the DeepL authentication key.
 func New(authKey string, opts ...ClientOption) *Client {
 	c := Client{
 		authKey: authKey,
@@ -34,47 +117,34 @@ func New(authKey string, opts ...ClientOption) *Client {
 	return &c
 }
 
-// A Client is a deepl client.
-type Client struct {
-	client       httpi.Client
-	authKey      string
-	baseURL      string
-	translateURL string
-}
-
-// A ClientOption configures the deepl client.
-type ClientOption func(*Client)
-
-// BaseURL sets the base url that is used for requests.
-func BaseURL(url string) ClientOption {
-	return func(c *Client) {
-		c.baseURL = url
-	}
-}
-
-// HTTPClient configures the *Client to use the given *http.Client for requests.
-func HTTPClient(client httpi.Client) ClientOption {
-	return func(c *Client) {
-		c.client = client
-	}
-}
-
-// HTTPClient returns the underlying *http.Client.
+// HTTPClient returns the underlying http.Client.
 func (c *Client) HTTPClient() httpi.Client {
 	return c.client
 }
 
-// BaseURL returns the configures base url for request.
+// BaseURL returns the configured base url for requests.
 func (c *Client) BaseURL() string {
 	return c.baseURL
 }
 
-// AuthKey returns the used authentication key.
+// AuthKey returns the DeepL authentication key.
 func (c *Client) AuthKey() string {
 	return c.authKey
 }
 
-// Translate translates the given text into the given targetLang.
+// Translate translates the provided text into the specified Language and
+// returns the translated text and the detected source Language of the input
+// text.
+//
+// When DeepL responds with an error, Translate returns an Error that contains
+// the DeepL error code and message. Use errors.As to unwrap the returned error
+// into an Error:
+//
+//	trans, sourceLang, err := c.Translate(context.TODO(), "Hello.", deepl.Japanese)
+//	var deeplError deepl.Error
+//	if errors.As(err, &deeplError) {
+//		log.Println(fmt.Sprintf("DeepL error code %d: %s", deeplError.Code, deeplError))
+//	}
 func (c *Client) Translate(ctx context.Context, text string, targetLang Language, opts ...TranslateOption) (string, Language, error) {
 	translations, err := c.TranslateMany(ctx, []string{text}, targetLang, opts...)
 	if err != nil {
@@ -88,10 +158,23 @@ func (c *Client) Translate(ctx context.Context, text string, targetLang Language
 	return translations[0].Text, Language(translations[0].DetectedSourceLanguage), nil
 }
 
-// TranslateMany translates multiple texts into the given targetLang.
+// TranslateMany translates the provided texts into the specified Language and
+// returns a Translation for every input text. The order of the translated texts
+// is guaranteed to be the same as the order of the input texts.
 //
-// Available options:
-//	SourceLang(), SplitSentences(), PreserveFormatting(), Formality()
+// When DeepL responds with an error, TranslateMany returns an Error that
+// contains the DeepL error code and message. Use errors.As to unwrap the
+// returned error into an Error:
+//
+//	translations, err := c.TranslateMany(
+//		context.TODO(),
+//		[]string{"Hello", "World"},
+//		deepl.Japanese,
+//	)
+//	var deeplError deepl.Error
+//	if errors.As(err, &deeplError) {
+//		log.Println(fmt.Sprintf("DeepL error code %d: %s", deeplError.Code, deeplError))
+//	}
 func (c *Client) TranslateMany(ctx context.Context, texts []string, targetLang Language, opts ...TranslateOption) ([]Translation, error) {
 	vals := make(url.Values)
 	vals.Set("auth_key", c.authKey)
@@ -129,48 +212,12 @@ func (c *Client) TranslateMany(ctx context.Context, texts []string, targetLang L
 	return response.Translations, nil
 }
 
-// A TranslateOption configures a translation.
-type TranslateOption func(url.Values)
-
-// SourceLang sets the source language of the text (`source_lang` option).
-func SourceLang(lang Language) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("source_lang", string(lang))
-	}
-}
-
-// SplitSentences sets the `split_sentences` option.
-func SplitSentences(split SplitSentence) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("split_sentences", split.Value())
-	}
-}
-
-// PreserveFormatting sets the `preserve_formatting` option.
-func PreserveFormatting(preserve bool) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("preserve_formatting", boolString(preserve))
-	}
-}
-
-// Formality sets the `formality` option.
-func Formality(formal Formal) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("formality", formal.Value())
-	}
-}
-
-// TagHandling sets the `tag_handling` option.
-func TagHandling(handling TagHandlingStrategy) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("tag_handling", handling.Value())
-	}
-}
-
-// IgnoreTags sets the `ignore_tags` option.
-func IgnoreTags(tags ...string) TranslateOption {
-	return func(vals url.Values) {
-		vals.Set("ignore_tags", strings.Join(tags, ","))
+func (err Error) Error() string {
+	switch err.Code {
+	case 456:
+		return "Quota exceeded. The character limit has been reached."
+	default:
+		return http.StatusText(err.Code)
 	}
 }
 
@@ -179,19 +226,4 @@ func boolString(b bool) string {
 		return "1"
 	}
 	return "0"
-}
-
-// Error is a deepl error.
-type Error struct {
-	// The HTTP error code, returned by the deepl API.
-	Code int
-}
-
-func (err Error) Error() string {
-	switch err.Code {
-	case 456:
-		return "Quota exceeded. The character limit has been reached."
-	default:
-		return http.StatusText(err.Code)
-	}
 }
