308d85c7a206a29fd2bee7fb426efc64e11fd53b
Michael Stapelberg <michael@stapelberg.de>
2021-09-16 22:50:09 +0200

    add methods for managing glossaries
    See https://www.deepl.com/docs-api/managing-glossaries/
    
    The glossary API requires authentication via the Authorization header (as
    opposed to a parameter).
    
    Also capture the body (contains error details) when the HTTP status code does
    not match our expectation. This is useful for recognizing when specifying an
    unsupported language combination, for example.
    
    fixes https://github.com/bounoable/deepl/issues/3

--- api.go
+++ api.go
@@ -1,5 +1,7 @@
 package deepl
 
+import "time"
+
 type translateResponse struct {
 	Translations []Translation `json:"translations"`
 }
@@ -8,4 +10,23 @@ type translateResponse struct {
 type Translation struct {
 	DetectedSourceLanguage string `json:"detected_source_language"`
 	Text                   string `json:"text"`
+}
+
+// Glossary as per
+// https://www.deepl.com/docs-api/managing-glossaries/creating-a-glossary/
+type Glossary struct {
+	GlossaryID   string    `json:"glossary_id"`
+	Name         string    `json:"name"`
+	Ready        bool      `json:"ready"`
+	SourceLang   string    `json:"source_lang"`
+	TargetLang   string    `json:"target_lang"`
+	CreationTime time.Time `json:"creation_time"`
+	EntryCount   int       `json:"entry_count"`
+}
+
+// A GlossaryEntry represents a single source→target entry in a glossary. This
+// is serialized to/from tab-separated values for DeepL.
+type GlossaryEntry struct {
+	Source string
+	Target string
 }
